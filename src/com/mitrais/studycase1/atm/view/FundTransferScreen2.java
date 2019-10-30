16332c54b87850b0549a783b1bd6af77b923c649
tuanl1803 <tuan_l1803@mitrais.com>
2019-10-30 09:44:50 +0700

    [Commit 3] Refactor: - Models - Service - View

--- src/com/mitrais/studycase1/atm/view/FundTransferScreen2.java
+++ src/com/mitrais/studycase1/atm/view/FundTransferScreen2.java
@@ -2,21 +2,20 @@ package com.mitrais.studycase1.atm.view;
 
 
 import com.mitrais.studycase1.atm.model.Account;
-import com.mitrais.studycase1.atm.service.FundTransferServices;
-import com.mitrais.studycase1.atm.service.FundTransferServicesImpl;
-import java.util.List;
+import com.mitrais.studycase1.atm.service.FundTransferService;
+import com.mitrais.studycase1.atm.service.FundTransferServiceImpl;
 
 public class FundTransferScreen2 {
 
-	public void show(List<Account> accounts) {
-		FundTransferServices fundTransferServices = new FundTransferServicesImpl();
+	public void show(Account account, Account desAccount) {
+		FundTransferService fundTransferService = new FundTransferServiceImpl();
 		
 		System.out.println();
 		System.out.print("Please enter transfer amount and \r\n" + 
 			         	 "Press enter to continue or \r\n" + 
-				         "Press cancel (Esc) to go back to Transaction: ");
+				         "Press cancel (Esc) to go back to Transfer: ");
 		
-		fundTransferServices.inputAmountScreen(accounts);
+		fundTransferService.inputAmountScreen(account, desAccount);
 		
 	}
 }
